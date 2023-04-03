0d95ab4cf85afad968182794767fb489213fa192
ausbosspoland <austinjpoland311@gmail.com>
2023-04-02 19:02:10 -0500

    tweaked color of navbar and changed the background picture

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-success">
+        <nav className="navbar navbar-expand-lg navbar-light bg-warning">
             <div className="container">
                 <a className="navbar-brand" href="#">Austin Poland</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
