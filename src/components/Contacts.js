cf305f9f2d742af86282839542e00677003f595e
ausbosspoland <austinjpoland311@gmail.com>
2023-05-22 05:01:42 -0500

    styled contact me component more and installed and integrated emailjs

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,6 +1,23 @@
 import React from "react";
+import emailjs from "emailjs-com";
 
 const Contacts = () => {
+
+    const serviceID = "service_ID";
+    const templateID = "template_ID";
+    const userID = "FJrSw1Jf3lmFgviUy";
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm(serviceID, templateID, e.target, userID)
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+    }
+
     return (
         <div className="contacts">
             <div className="text-center">
@@ -8,6 +25,7 @@ const Contacts = () => {
             <p>Please fill out the form and describe your interests or concerns and I will contact you as soon as possible.</p>
             </div>
             <div className="container">
+                <form onSubmit={sendEmail}>
                 <div className="row">
                     <div className="col-md-6 col-xs-12">
                         {/* Name Input */}
@@ -18,30 +36,34 @@ const Contacts = () => {
                             placeholder="Name"
                             name="name"
                         />
+                        <div className="line"></div>
                         {/* Phone Input */}
                         <input
                         id="phone"
                         type="text"
                             className="form-control"
                             placeholder="Phone Number"
-                            phone="phone"
+                            name="phone"
                         />
+                        <div className="line"></div>
                         {/* Email Input */}
                         <input
                         id="email"
                         type="email"
                             className="form-control"
                             placeholder="Email"
-                            email="email"
+                            name="email"
                         />
+                        <div className="line"></div>
                         {/* Subject Input */}
                         <input
                         id="subject"
                         type="text"
                             className="form-control"
                             placeholder="Subject"
-                            subject="subject"
+                            name="subject"
                         />
+                        <div className="line"></div>
                     </div>
                     <div className="col-md-6 col-xs-12">
                         {/* Description */}
@@ -50,11 +72,13 @@ const Contacts = () => {
                         type="text"
                         className="form-control"
                         placeholder="Email"
-                        email="email"
+                        name="email"
                         ></textarea>
+                        <div className="line"></div>
                         <button className="btn-main-offer contact-btn" type="submit">contact me</button>
                     </div>
                 </div>
+                </form>
             </div>
         </div>
     )
